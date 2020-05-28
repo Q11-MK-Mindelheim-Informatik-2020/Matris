2dc0a0abb82d82a3f653e4ca9a7a2744617ef84c
Elias Schmid <eliasmaristenkolleg@gmail.com>
2020-05-28 20:56:43 +0200

    Commit: UI Update         -Changed Background Texture         -Enhanced user experience         -Removed Herobrine

--- src/Var/Var.java
+++ src/Var/Var.java
@@ -10,13 +10,13 @@ public class Var {
     public static int currentid = 1; //ID des aktiven/fallenden Blöcken
     public static String gameState = "startup";
     public static int fps = 60;
-    public static int m = 15; //Höhe
+    public static int m = 20; //Höhe
     public static int n = 10; //Breite
     public static int tilesX = n; //Anzahl der Felder in x Richtung
     public static int tilesY = m; //Anzahl der Felder in y Richtung
-    public static int tilePadding = 5; //Felderabstand
-    public static int tileStartX = 438, tileStartY = 622; //Startposition Spielfeld
-    public static int tileSize = 36; //Kantenlänge der Felder
+    public static int tilePadding = 3; //Felderabstand
+    public static int tileStartX = 472, tileStartY = 646; //Startposition Spielfeld
+    public static int tileSize = 31; //Kantenlänge der Felder
     public static Box spielfeld[][] = new Box[n][m]; //Spielfeld mit Box-Objekten
     public static int width = 1280, height = 720;
     public static Color backgroundColor = new Color(24, 21, 33);
